3e6cddabca04a4f5234ebeafba8468d1f88941a3
Jason Ou <jason.ou.pro@gmail.com>
2023-07-17 16:24:49 +0800

    Add domain structure

--- Sources/QRCode/QRCode.swift
+++ Sources/QRCode/QRCode.swift
@@ -1,6 +1,43 @@
-public struct QRCode {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
 
-    public init() {
+public struct QRCode {
+    public let url: URL
+    public let size: CGSize
+    public let color: Color
+    public let overlay: Overlay?
+    
+    public init(
+        url: URL,
+        size: CGSize = CGSize(width: 300, height: 300),
+        color: Color = Color(foreground: .black, background: .white),
+        overlay: Overlay? = nil
+    ) {
+        self.url = url
+        self.size = size
+        self.color = color
+        self.overlay = overlay
+    }
+}
+
+extension QRCode {
+    public struct Color {
+        public let foreground: UIColor
+        public let background: UIColor
+        
+        public init(foreground: UIColor, background: UIColor) {
+            self.foreground = foreground
+            self.background = background
+        }
+    }
+    
+    public struct Overlay {
+        public let size: CGSize
+        public let image: UIImage
+        
+        public init(size: CGSize, image: UIImage) {
+            self.size = size
+            self.image = image
+        }
     }
 }
