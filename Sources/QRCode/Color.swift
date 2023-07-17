bb15caeeeecfdd932b7b7b53e3380cf89c8cd84a
Jason Ou <jason.ou.pro@gmail.com>
2023-07-17 16:32:07 +0800

    Define Color and Overlay

---
+++ Sources/QRCode/Color.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+public struct Color {
+    public let foreground: UIColor
+    public let background: UIColor
+    
+    public init(foreground: UIColor, background: UIColor) {
+        self.foreground = foreground
+        self.background = background
+    }
+}
---
+++ Sources/QRCode/Overlay.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+public struct Overlay {
+    public let size: CGSize
+    public let image: UIImage
+    
+    public init(size: CGSize, image: UIImage) {
+        self.size = size
+        self.image = image
+    }
+}
